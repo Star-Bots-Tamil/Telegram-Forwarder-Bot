0990d996624d351a825320e43270aa2a439c1888
Star Bots Tamil <126967523+Star-Bots-Tamil@users.noreply.github.com>
2023-11-24 05:13:03 +0530

    Update misc.py

--- forwarder/modules/misc.py
+++ forwarder/modules/misc.py
@@ -1,51 +1,36 @@
 from telegram import Update
 from telegram.constants import ParseMode
 from telegram.ext import filters, MessageHandler
-
 from forwarder import OWNER_ID, bot
-
 
 async def get_id(update: Update, _):
     message = update.effective_message
-    if not message:
-        return
-
-    if message.reply_to_message:
-        if message.reply_to_message.forward_from:  # Forwarded user
-            sender = message.reply_to_message.forward_from
-            forwarder = message.reply_to_message.from_user
-            return await message.reply_text(
-                f"🙋‍♂️ The original sender ({sender.first_name}), ID is: `{sender.id}`\n"
-                f"⏩ The forwarder ({forwarder.first_name if forwarder else 'Unknown'}) ID: `{forwarder.id if forwarder else 'Unknown'}`",
-                parse_mode=ParseMode.MARKDOWN,
-            )
-
-        if message.reply_to_message.forward_from_chat:  # Forwarded channel
-            channel = message.reply_to_message.forward_from_chat
-            forwarder = message.reply_to_message.from_user
-            return await message.reply_text(
-                f"💬 The channel {channel.title} ID: `{channel.id}`\n"
-                f"⏩ The forwarder ({forwarder.first_name if forwarder else 'Unknown'}) ID: `{forwarder.id if forwarder else 'Unknown'}`",
-            )
-
-        user = message.reply_to_message.from_user
-        return await message.reply_text(
-            f"🙋‍♂️ {user.first_name if user else 'Unknown'} ID: `{user.id if user else 'Unknown'}`"
-        )
-
     chat = update.effective_chat
-    if not chat:
+    if not message or not chat:
         return
 
     if chat.type == "private":  # Private chat with the bot
-        return await message.reply_text(f"🙋‍♂️ Your ID is `{chat.id}`.")
+        return await message.reply_text(f"<b>💁🏻 Your ID is</b> <code>{chat.id}</code>", parse_mode=ParseMode.HTML)
 
-    result = f"👥 Chat ID: {chat.id}"
+    result = f"<b>👥 Chat ID :-</b> <code>{chat.id}</code>", parse_mode=ParseMode.HTML
     if chat.is_forum:
-        result += f"\n💬 Forum/Topic ID: {message.message_thread_id}"
+        result += f"<b>\n💬 Forum/Topic ID :-<b> <code>{message.message_thread_id}</code>", parse_mode=ParseMode.HTML
+
+    if message.reply_to_message:
+        forwarder = message.reply_to_message.from_user
+        if message.reply_to_message.forward_from:  # Forwarded user
+            sender = message.reply_to_message.forward_from
+            result += f"<b>💁🏻 The Original Sender ({sender.first_name}), ID is :-</b> <code>{sender.id}</code>\n", parse_mode=ParseMode.HTML
+            result += f"<b>⏩ The Forwarder ({forwarder.first_name if forwarder else 'Unknown'}) ID :-</b> <code>{forwarder.id if forwarder else 'Unknown'}</code>", parse_mode=ParseMode.HTML
+
+        if message.reply_to_message.forward_from_chat:  # Forwarded channel
+            channel = message.reply_to_message.forward_from_chat
+            result += f"<b>💬 The Channel {channel.title} ID :-</b> <code>{channel.id}</code>\n", parse_mode=ParseMode.HTML
+            result += f"<b>⏩ The Forwarder ({forwarder.first_name if forwarder else 'Unknown'}) ID :-</b> <code>{forwarder.id if forwarder else 'Unknown'}</code>", parse_mode=ParseMode.HTML
+
     return await message.reply_text(
         result,
-        parse_mode=ParseMode.MARKDOWN,
+        parse_mode=ParseMode.HTML,
     )
 
 
